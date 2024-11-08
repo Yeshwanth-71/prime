7e7d9b4d941575486fddc767019399bd32aeb448
Yeshwanth <reddyyeshwanth720@gmail.com>
2024-11-08 12:13:39 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Login from './Pages/Login'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouterRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css'
 import Dashboard from './Pages/Dashboard';
 import Telmovies from './Pages/Telmovies';
